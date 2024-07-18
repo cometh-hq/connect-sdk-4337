f1beeba0624bdbc6716fd22cd0839acfb2a23fb4
yoanslvy <97676023+yoanslvy@users.noreply.github.com>
2024-07-18 16:00:18 +0200

    update: archi

---
+++ examples/example/src/app/page.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import React, { useState } from "react";
+
+import ConnectWallet from "./components/ConnectWallet";
+import Transaction from "./components/Transaction";
+import { useSmartAccount } from "./modules/hooks/useSmartAccount";
+
+export default function App() {
+    const {
+        isConnecting,
+        isConnected,
+        connect,
+        connectionError,
+        smartAccount,
+        newSigner,
+    } = useSmartAccount();
+    const [transactionSuccess, setTransactionSuccess] = useState(false);
+
+    const validate = async () => {
+        const signer = {
+            signerAddress: "0x5929d4e3d17318BaB73026644A904AdbC6fa7989",
+            deviceData: {
+                browser: "Firefox",
+                os: "macOS",
+                platform: "desktop",
+            },
+        };
+
+        await smartAccount.validateAddDevice({ signer });
+    };
+
+    return (
+        <div
+            style={{
+                height: "100vh",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <div className="md:min-h-[70vh] gap-2 flex flex-col justify-center items-center">
+                <div className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
+                    <div className="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
+                        <div className="grid divide-gray-900/5 bg-gray-50">
+                            <ConnectWallet
+                                isConnected={isConnected}
+                                isConnecting={isConnecting}
+                                connect={connect}
+                                connectionError={connectionError}
+                                smartAccount={smartAccount!}
+                            />
+                        </div>
+
+                        {isConnected && (
+                            <>
+                                <Transaction
+                                    smartAccount={smartAccount}
+                                    transactionSuccess={transactionSuccess}
+                                    setTransactionSuccess={
+                                        setTransactionSuccess
+                                    }
+                                />
+
+                                <button onClick={validate}>
+                                    Validate Add Device
+                                </button>
+                            </>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
