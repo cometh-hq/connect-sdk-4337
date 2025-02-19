5146b9da9fb1c45091d202d11e482190ef17ec74
yoanslvy <97676023+yoanslvy@users.noreply.github.com>
2025-02-19 11:24:56 +0100

    fix: bitwarden

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -49,6 +49,7 @@ import type {
     CreateSessionDataParams,
     GrantPermissionResponse,
 } from "./core/modules/sessionKey/types";
+import { providerToSmartAccountSigner } from "./core/signers/utils";
 
 export {
     createSigner,
@@ -67,6 +68,7 @@ export {
     erc7579Actions,
     smartSessionActions,
     toSmartSessionsSigner,
+    providerToSmartAccountSigner,
     ENTRYPOINT_ADDRESS_V07,
     customChains,
     SmartSessionMode,
